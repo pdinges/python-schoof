f39e450b05dd792d621bebb6822e5f39765aa186
Peter Dinges <pdinges@acm.org>
2010-04-17 18:50:46 +0000

    Added API documentation.

--- rings/__init__.py
+++ rings/__init__.py
@@ -1,51 +1,274 @@
 # -*- coding: utf-8 -*-
 # $Id$
 
+"""
+Ring related classes and functions (ring as in integers).
+
+@package   rings
+@author    Peter Dinges <me@elwedgo.de>
+"""
+
 class CommutativeRing:
     """
-    Base class for elements from a commutative ring; it provides default
+    A base class for elements of commutative rings that provides default
     operator overloading.
     
     For example, subtraction is defined as addition of the additive inverse.
-    Derived classes therefore only have to implement these operations if a
-    shortcut is available.
+    Derived classes therefore only have to implement the following operations
+    to support the complete operator set:
+    - __bool__(): Zero testing (@c True if not zero)
+    - __eq__(): Equality testing with the @c == operator (@c True if equal)
+    - __add__(): Addition with the @c + operator; @c self is the left summand
+    - __neg__(): Negation with the @c - unary minus operator (the additive inverse)
+    - __mul__(): Multiplication with the @c * operator; @c self is the
+      left factor
+    - __divmod__(): Division with remainder; @c self is the dividend (left element)
+    
+    The following operations are implemented in terms of above operations using
+    the field axioms:
+    - __neq__(): Inequality testing with the @c != operator
+    - __radd__(): Addition with the @c + operator; @c self is the right summand
+    - __sub__(): Subtraction with the @c - operator; @c self is the minuend
+      (left element)
+    - __rsub__(): Subtraction with the @c - operator; @c self is the
+      subtrahend (right element)
+    - __rmul__(): Multiplication with the @c * operator; @c self is the
+      right factor
+    - __rdivmod__(): Division with remainder using @c divmod(); @c self is the
+      divisor (right element)
+    - __floordiv__(): Remainder ignoring division with the @c // operator;
+      @c self is the dividend (left element) 
+    - __rfloordiv__(): Remainder ignoring division with the @c // operator;
+      @c self is the divisor (right element) 
+    - __mod__(): Remainder of @c self modulo @c other with the @c % operator
+    - __rmod__(): Remainder of @c other modulo @c self with the @c % operator
+    - __pow__(): Exponentiation with integers
     """
-    # TODO: Raise NotImplementedError on required operations?
+    
+    #- Template Operations ---------------------------------------------------- 
     
     def __neq__(self, other):
+        """
+        Test whether another element @p other is different from @p self; return
+        @c True if that is the case.  The infix operator @c != calls this
+        method; for example:
+        @code
+        if self != other:
+            do_something()
+        @endcode
+        """
         return not self.__eq__( other )
     
     def __radd__(self, other):
+        """
+        Return the sum of @p self and @p other.  The infix operator @c + calls
+        this method if @p self is the right summand and @p other.__add__()
+        returns @c NotImplemented. For example:
+        @code
+        result = other + self
+        @endcode
+        """
         # Additive subgroup is commutative:
         # other + self == self + other
         return self.__add__( other )
     
     def __sub__(self, other):
+        """
+        Return the difference of @p self and @p other.  The infix operator @c -
+        calls this method if @p self is the minuend (left element); for example:
+        @code
+        result = self - other
+        @endcode
+        """
         return self.__add__( -other )
     
     def __rsub__(self, other):
+        """
+        Return the difference of @p other and @p self.  The infix operator @c -
+        calls this method if @p self is the subtrahend (right element) and
+        @c other.__sub__() returns @c NotImplemented.  For example:
+        @code
+        result = other - self
+        @endcode
+        """
         # Additive subgroup is commutative:
         # other - self == -( self - other )
         return -self.__sub__( other )
     
     def __rmul__(self, other):
+        """
+        Return the product of @p self and @p other.  The infix operator @c *
+        calls this method if @p self is the right factor and @c other.__mul__()
+        returns @c NotImplemented.  For example:
+        @code
+        result = other * self
+        @endcode 
+        """
         # Commutative ring:
         # other * self == self * other
         return self.__mul__( other )
 
     def __rdivmod__(self, other):
+        """
+        Return quotient and remainder of @p other divided by @p self.  The
+        @c divmod() built-in function calls this method if @p self is the
+        divisor and @c other.__divmod__() returns @c NotImplemented.
+        For example:
+        @code
+        quotient, remainder = divmod( other, self )
+        @endcode
+        """
         return divmod( self.__class__( other ), self )
     
+    def __floordiv__(self, other):
+        """
+        Return the quotient of @p self divided by @p other and ignore the
+        remainder.  The @c // operator calls this method; for example:
+        @code
+        quotient = self // other
+        @endcode
+        """
+        return divmod( self, other )[0]
+
     def __rfloordiv__(self, other):
+        """
+        Return the quotient of @p self divided by @p other and ignore the
+        remainder.  The @c // operator calls this method if @p self is the
+        divisor and @c other.__floordiv__() returns @c NotImplemented.
+        For example:
+        @code
+        quotient = other // self
+        @endcode
+        """
         return divmod( other, self )[0]
     
+    def __mod__(self, other):
+        """
+        Return @p self modulo @p other, that is, the remainder of @p self
+        divided by @p other.  The @c % operator calls this method; for example:
+        @code
+        remainder = self % other
+        @endcode 
+        """
+        return divmod( self, other )[1]
+
     def __rmod__(self, other):
+        """
+        Return @p other modulo @p self, that is, the remainder of @p other
+        divided by @p self.  The @c % operator calls this method if @p self is
+        the divisor and @c other.__mod__() returns @c NotImplemented.
+        For example:
+        @code
+        remainder = other % self
+        @endcode 
+        """
         return divmod( other, self )[1]
     
-    def __pow__(self, other):
+    def __pow__(self, n):
+        """
+        Return @p self taken to the @p n-th power.  The infix operator @c **
+        calls this method; for example:
+        @code
+        result = self ** n
+        @endcode
+        
+        @note  The implementation uses the most naive by-the-book method for
+               exponentiation: the element is multiplied @p n-1 times with
+               itself.  This is slow (and might skin your cat!).  However, the
+               purpose of this code is to be easy to understand, not fast. 
+        
+        @param n   The exponent; it is expected to be a non-negative integer
+                   type.  Negative integers and floats are unsupported.
+        """
         # This only makes sense for integer arguments.
         result = self
-        for i in range(1, int(other)):
+        for i in range(1, int(n)):
             result = result * self
         
         return result
+
+
+    #- Base Operations (Defined in Derived Classes) ---------------------------
+    
+    def __bool__(self):
+        """
+        Test whether the element is non-zero: return @c True if, and only if,
+        it is non-zero. Otherwise return @c False.  Implicit conversions to
+        boolean (truth) values use this method; for example when @c x is an
+        element of a CommutativeRing:
+        @code
+        if x:
+            do_something()
+        @endcode
+        
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
+    
+    def __eq__(self, other):
+        """
+        Test whether another element @p other is equal to @p self; return
+        @c True if that is the case.  The infix operator @c == calls this
+        method; for example:
+        @code
+        if self == other:
+            do_something()
+        @endcode
+        
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
+    
+    def __add__(self, other):
+        """
+        Return the sum of @p self and @p other.  The infix operator @c + calls
+        this method if @p self is the left summand; for example:
+        @code
+        result = self + other
+        @endcode
+        
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
+    
+    def __neg__(self):
+        """
+        Return the additive inverse of @p self.  The unary minus operator @c -x
+        calls this method; for example:
+        @code
+        negated = -self
+        @endcode
+
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
+    
+    def __mul__(self, other):
+        """
+        Return the product of @p self and @p other.  The infix operator @c + calls
+        this method if @p self is the left factor; for example:
+        @code
+        result = self * other
+        @endcode
+        
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
+    
+    def __divmod__(self, other):
+        """
+        Return quotient and remainder of @p self divided by @p other.  The
+        @c divmod() built-in function calls this method; for example:
+        @code
+        quotient, remainder = divmod( self, other )
+        @endcode
+
+        @exception NotImplementedError if this method is called; subclasses
+                                       must implement this operation.
+        """
+        raise NotImplementedError
--- rings/integers/naive.py
+++ rings/integers/naive.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 # $Id$
 
+"""
+A wrapper that extends Python's built-in integer type @c int with the expected
+ring interface.
+
+@package   rings.integers.naive
+@author    Peter Dinges <me@elwedgo.de>
+"""
+
 from support.profiling import profiling_name
 
 @profiling_name("Z")
 class Integers(int):
     """
-    Ring of integers.
+    The ring of integers.  This wrapper class extends Python's built-in integer
+    type @c int with the interface provided by the other ring classes.
     
-    This wrapper class makes python's integers available under the interface
-    for rings.  
+    @see   rings.polynomials.naive.Polynomials,
+           rings.quotients.naive.QuotientRing
     """
     @staticmethod
     def zero():
+        """
+        Return @c 0, the neutral element of integer addition.
+        """
         return 0
 
     @staticmethod
     def one():
+        """
+        Return @c 1, the neutral element of integer multiplication.
+        """
         return 1
